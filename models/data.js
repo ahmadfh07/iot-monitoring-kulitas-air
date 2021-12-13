74ff21ad08693ddb338842a8fcb82153b711f22b
Ahmad Faqih Hidayat <ahmadfh36115@gmail.com>
2021-12-13 20:16:35 +0700

    date and time bug solved

--- models/data.js
+++ models/data.js
@@ -1,18 +1,13 @@
 const mongoose = require("mongoose");
-
-const Data = mongoose.model("Data", {
-  date: {
-    type: Date,
-    default: new Date(),
+const dataSchema = mongoose.Schema(
+  {
+    tds: {
+      type: String,
+      required: true,
+    },
   },
-  time: {
-    type: Date,
-    default: new Date(),
-  },
-  tds: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
+const Data = mongoose.model("Data", dataSchema);
 
 module.exports = Data;
